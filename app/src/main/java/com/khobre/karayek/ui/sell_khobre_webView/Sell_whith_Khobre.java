33a78743a6ce13aaf0280eec23fa2947bffedd4c
amin asvadi <amin.asvadi@gmail.com>
2020-09-07 17:41:17 +0430

    BUG

--- app/src/main/java/com/khobre/karayek/ui/sell_khobre_webView/Sell_whith_Khobre.java
+++ app/src/main/java/com/khobre/karayek/ui/sell_khobre_webView/Sell_whith_Khobre.java
@@ -19,15 +19,22 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.khobre.karayek.util.IabHelper;
+import com.khobre.karayek.util.IabResult;
+import com.khobre.karayek.util.Inventory;
+import com.khobre.karayek.util.Purchase;
 import com.khobre.karayek.R;
 import com.khobre.karayek.ui.Network.ApiClient;
 import com.khobre.karayek.ui.Network.RetrofitApiInterface;
 import com.khobre.karayek.ui.databse.DbSql;
+import com.khobre.karayek.ui.home.HomeFragment;
 import com.khobre.karayek.ui.model.PaymentModel;
 import com.khobre.karayek.ui.model.Price;
 import com.khobre.karayek.ui.model.Users;
 import com.khobre.karayek.ui.sahmList.SahamListModel;
+import com.khobre.karayek.util.IabHelper;
+import com.khobre.karayek.util.IabResult;
+import com.khobre.karayek.util.Purchase;
 import com.zarinpal.ewallets.purchase.OnCallbackRequestPaymentListener;
 import com.zarinpal.ewallets.purchase.OnCallbackVerificationPaymentListener;
 import com.zarinpal.ewallets.purchase.PaymentRequest;
@@ -60,6 +67,21 @@ public class Sell_whith_Khobre extends AppCompatActivity {
     String uri = "http://45.149.77.68/saham_edalat/";
     RetrofitApiInterface request;
     int price;
+    private boolean mBazaarInstalled;
+
+    // SKUs for our products: the premium upgrade (non-consumable)
+    static final String SKU_PREMIUM = "register";
+
+    // Does the user have the premium upgrade?
+    boolean mIsPremium = false;
+
+    // (arbitrary) request code for the purchase flow
+    static final int RC_REQUEST = 1;
+    private static final String TAG = Sell_whith_Khobre.class.getSimpleName() + "TAG";
+
+    // The helper object
+    IabHelper mHelper;
+
     TextView priceText;
 
     @Override
@@ -75,9 +97,11 @@ public class Sell_whith_Khobre extends AppCompatActivity {
         price = Integer.valueOf(paymentModelsItems.get(0).getPrice());
 
 
+
+
         init();
 
-  data =getIntent().getData();
+/* data =getIntent().getData();
 
         if (data!=null) {
             ZarinPal.getPurchase(this).verificationPayment(data, new OnCallbackVerificationPaymentListener() {
@@ -99,7 +123,6 @@ public class Sell_whith_Khobre extends AppCompatActivity {
                     dialogOk.show();
                         ;
 
-                       /* Toast.makeText(Sell_whith_Khobre.this,  phoneStr +refID, Toast.LENGTH_SHORT).show();*/
                         //tv.setText( "موفق " + refID);
                         //   Log.i("TAG", refID);
                         Toast.makeText(Sell_whith_Khobre.this, "موفق", Toast.LENGTH_LONG).show();
@@ -137,8 +160,7 @@ public class Sell_whith_Khobre extends AppCompatActivity {
             });
         }else {
             //tv.setText("recieved data is ");
-           /* Toast.makeText(this, "recieved data is null", Toast.LENGTH_SHORT).show();*/
-        }
+        }*/
 
         btn_byt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -266,8 +288,9 @@ call.enqueue(new Callback<ResponseBody>() {
                     public void onClick(View v) {
 
 
-
-
+                        String payload = "";
+                        mHelper.launchPurchaseFlow(Sell_whith_Khobre.this, SKU_PREMIUM, RC_REQUEST,
+                                mPurchaseFinishedListener, payload);
                         dialog.dismiss();
 
                         // This is for ZarinPal Payment
@@ -319,6 +342,155 @@ call.enqueue(new Callback<ResponseBody>() {
                 finish();
             }
         });
+
+        String base64EncodedPublicKey = " MIHNMA0GCSqGSIb3DQEBAQUAA4G7ADCBtwKBrwDq04CFsL0WCAOIATc1u4QxyjI32BkZ4RwNFDQs8i4TQXCnsD0/5cfYlf07Vnm9e22Xlh+Hm+CYpaJw7kCwSyqRZ0kWENeAx8sWMR2LTcAsahJEoQX1rw5UVsQlNOdf3AhwfZejsQtzFB+RenT95hwz9QlBgT+d6GaikJ4XPgIN6TkVk53DFNw+5zmumbwgY94zGugbJCTJ0UtPZELUIG0GuY5XlbvNsd4RD4PmwXsCAwEAAQ== ";
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+        mHelper.enableDebugLogging(true);
+        Log.d(TAG, "Starting setup.");
+
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                //mProgressDialog.dismiss();
+                mBazaarInstalled = true;
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    // moshkeli dar ertebat ba bazaar pish amad
+                    Log.e("TAG", "failed to access bazaar " + result);
+
+                    Log.e(TAG, "Problem setting up In-app Billing: " + result);
+
+                }
+
+                mHelper.queryInventoryAsync(mGotInventoryListener);
+            }
+        });
+    }
+    ////////////////////////FOR BAZAAR CLASS ////////////////////////////////////
+
+
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+            if (mHelper == null) return;
+
+            if (result.isFailure()) {
+                // moshkeli dar ertebat ba bazaar pish amad
+                Log.e("TAG", "failed to access bazaar " + result.getMessage());
+                Toast.makeText(context, "خطایی در ارتباط با بازار رخ داد! از وصل بودن اینترنت خود مطمئن شوید", Toast.LENGTH_SHORT).show();
+                //neshan dad dialog khata be karbar(dar inja sweet alert)
+                /*new SweetAlertDialogCustom(BuyActivity.this, SweetAlertDialogCustom.CUSTOM_IMAGE_TYPE)
+                        .setTitleText("کاربر گرامی")
+                        .setContentText("خطایی در ارتباط با بازار رخ داد! از وصل بودن اینترنت خود مطمئن شوید.")
+                        .setConfirmClickListener(new SweetAlertDialogCustom.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialogCustom sDialog) {
+                                finish();
+                            }
+                        })
+                        .show();*/
+                return;
+            }
+            Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
+            mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+            Log.d(TAG, "Query inventory was successful.");
+            // does the user have the premium upgrade?
+            //aya karbar noskhe premium ra ghablan kharide ast?
+            mIsPremium = inventory.hasPurchase(SKU_PREMIUM);
+            // update UI accordingly
+            //agar kharide ast bayad mohtava barayash baz shavad
+
+            Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+
+
+            Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         *
+         * WARNING: Locally generating a random string when starting a purchase and
+         * verifying it here might seem like a good approach, but this will fail in the
+         * case where the user purchases an item on one device and then uses your app on
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         *
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         *
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on
+         *    one device work on other devices owned by the user).
+         *
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+
+        return true;
+    }
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            if (result.isFailure()) {
+                // moshkeli dar ertebat ba bazaar pish amad
+                Log.e("TAG", "failed to access bazaar " + result);
+                //neshan dad dialogE khata be karbar(dar inja sweet alert)
+                /*new SweetAlertDialogCustom(BuyActivity.this, SweetAlertDialogCustom.CUSTOM_IMAGE_TYPE)
+                        .setTitleText("کاربر گرامی")
+                        .setContentText("خطایی در ارتباط با بازار رخ داد! از وصل بودن اینترنت خود مطمئن شوید.")
+                        .setConfirmClickListener(new SweetAlertDialogCustom.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialogCustom sDialog) {
+                                finish();
+                            }
+                        })
+                        .show();*/
+                return;
+            } else if (purchase.getSku().equals(SKU_PREMIUM)) {
+                insertToDatabseOnline(nameStr,phoneStr,personIdStr);
+                Dialog dialogacc = new Dialog(Sell_whith_Khobre.this);
+                dialogacc.setContentView(R.layout.layout_accepted_dialog);
+                btn_ok = dialogacc.findViewById(R.id.btn_positive_accept);
+                btn_ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialogacc.dismiss();
+                    }
+                });
+                dialogacc.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialogacc.show();
+                // give user access to premium content and update the UI
+                // be karbar dastrasi be mohtavaye premium(puli) ra bedahid. karbar kharid ra kamel anjam dad
+               /* Intent intent = new Intent(BuyActivity.this, MainActivity.class);
+                startActivity(intent);*/
+            }
+        }
+    };
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass on the activity result to the helper for handling
+        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        } else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mHelper != null && mBazaarInstalled) mHelper.dispose();
+        mHelper = null;
     }
 
 }
