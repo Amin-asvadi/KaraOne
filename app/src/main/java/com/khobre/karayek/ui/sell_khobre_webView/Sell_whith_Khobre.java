6545d3da245d69dcc80ebbe46e3cc98b6c12cc57
amin asvadi <amin.asvadi@gmail.com>
2020-09-10 12:58:54 +0430

    complete project

--- app/src/main/java/com/khobre/karayek/ui/sell_khobre_webView/Sell_whith_Khobre.java
+++ app/src/main/java/com/khobre/karayek/ui/sell_khobre_webView/Sell_whith_Khobre.java
@@ -158,9 +158,14 @@ public class Sell_whith_Khobre extends AppCompatActivity {
                 return;
             }else {
 
-                mIsPremium = inventory.hasPurchase(SKU_PREMIUM);
-
-
+                Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
+                mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+                Purchase gasPurchase = inventory.getPurchase(SKU_PREMIUM);
+                if (gasPurchase != null && verifyDeveloperPayload(gasPurchase)) {
+                    Log.d(TAG, "We have gas. Consuming it.");
+                    mHelper.consumeAsync(inventory.getPurchase(SKU_PREMIUM), mConsumeFinishedListener);
+                    return;
+                }
                 Log.d(TAG, "Initial inventory query finished; enabling main UI.");
             }
 
@@ -170,7 +175,34 @@ public class Sell_whith_Khobre extends AppCompatActivity {
     };
 
 
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
 
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         *
+         * WARNING: Locally generating a random string when starting a purchase and
+         * verifying it here might seem like a good approach, but this will fail in the
+         * case where the user purchases an item on one device and then uses your app on
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         *
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         *
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on
+         *    one device work on other devices owned by the user).
+         *
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+
+        return true;
+    }
     IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
         public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
             if (result.isFailure()) {
